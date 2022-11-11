2c09caf49e5f16ab1906f2d3894b8253d2dc2827
juliette barbay <barbayjuliette95@gmail.com>
2022-11-11 15:09:36 +0800

    Database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.destroy_all
+
+puts "Creating 100 movies"
+
+100.times do
+  movie = Movie.new(title: Faker::Movie.unique.title,
+                    overview: Faker::Movie.quote,
+                    poster_url: "#{Faker::TvShows::HowIMetYourMother.character}.com",
+                    rating: rand(0.1..9.1).round(2))
+  movie.save!
+end
+
+puts "All movies created"
