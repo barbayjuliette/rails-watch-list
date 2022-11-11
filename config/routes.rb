2ced35904fa60f018ec99f9bbc11ca78d8d888e6
juliette barbay <barbayjuliette95@gmail.com>
2022-11-11 17:46:08 +0800

    add movies to list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
 end
